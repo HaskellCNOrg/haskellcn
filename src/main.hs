39706134d4f4aca06469522e95f8b86c1591f7dd
Haisheng Wu mac16 <freizl@gmail.com>
2021-06-01 11:05:05 -0700

    Upgrade cabal; change dist folder name;
    - remove irc and weibo from main page
    - small tweak to main page

--- src/main.hs
+++ src/main.hs
@@ -1,30 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import           Prelude         hiding (id)
 import           Hakyll
+import           Prelude                 hiding ( id )
+
+hackyllConfig :: Configuration
+hackyllConfig = defaultConfiguration { destinationDirectory = "_netlify-site" }
 
 main :: IO ()
-main = hakyllWith defaultConfiguration $ do
+main = hakyllWith hackyllConfig $ do
 
     -- Compress CSS
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+  match "css/*" $ do
+    route idRoute
+    compile compressCssCompiler
 
-    -- Static directories
-    match ("images/*" .||. "font/**/*" .||. "_redirects") $ do
-        route   idRoute
-        compile copyFileCompiler
+  -- Static directories
+  match ("images/*" .||. "font/**/*" .||. "_redirects") $ do
+    route idRoute
+    compile copyFileCompiler
 
-    -- Render Index
-    create ["index.html"] $ do
-      route idRoute
-      compile $ do
-        makeItem ""
-          >>= loadAndApplyTemplate "templates/index.html" defaultContext
-          >>= loadAndApplyTemplate "templates/default.html" defaultContext
-          >>= relativizeUrls
+  -- Render Index
+  create ["index.html"] $ do
+    route idRoute
+    compile $ do
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/index.html"   defaultContext
+        >>= loadAndApplyTemplate "templates/default.html" defaultContext
+        >>= relativizeUrls
 
-    -- Read templates
-    match "templates/*" $ compile templateCompiler
+  -- Read templates
+  match "templates/*" $ compile templateCompiler
